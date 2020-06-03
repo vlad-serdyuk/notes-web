6fcea3318c6f9d8adcfefa90f79380f7c509cc34
vlad.serdyuk <vladserduk@live.com>
2020-06-03 16:05:40 +0300

    Add Nav & Header components

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export const Home = () => {
   return (
     <div>
       <h1>Notes</h1>
       <p>Home page</p>
+      <ul>
+        <li><Link to="/my-notes">My Notes</Link></li>
+        <li><Link to="/favorites">Favorites</Link></li>
+      </ul>
     </div>
   );
 };
