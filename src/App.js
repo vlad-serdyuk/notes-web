bdbd40e26bc8870541ad07463cc8e1e384895030
vlad.serdyuk <vladserduk@live.com>
2020-06-05 17:08:58 +0300

    Add apollo wrapper

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Grommet } from 'grommet';
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client'; 
 
 import { Pages } from './pages';
 import GlobalStyles from './styled/globalStyles';
 
+const uri = process.env.API_URI;
+const cache = new InMemoryCache();
+
+const client = new ApolloClient({
+  uri,
+  cache,
+  connectToDevTools: true,
+});
+
 const App = () => {
   return (
-    <Grommet plain>
-      <GlobalStyles />
-      <Pages />
-    </Grommet>
+    <ApolloProvider client={client}>
+      <Grommet plain>
+        <GlobalStyles />
+        <Pages />
+      </Grommet>
+    </ApolloProvider>
   );
 };
 
