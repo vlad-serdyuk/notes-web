cf5de5a0a9fa7d84b021373dc34d22029abfe29a
vlad.serdyuk <vladserduk@live.com>
2020-06-24 22:26:38 +0300

    Fix updating cache after add a new note

--- src/pages/CreateNote/CreateNote.js
+++ src/pages/CreateNote/CreateNote.js
@@ -2,10 +2,11 @@ import React, { Fragment, useCallback } from 'react';
 import { useMutation } from '@apollo/client';
 
 import { NoteForm } from '../../components/NoteForm/NoteForm';
-import { CREATE_NOTE } from '../../gql/query';
+import { CREATE_NOTE, GET_NOTES } from '../../gql/query';
 
 const CreateNotePage = ({ history }) => {
   const [createNote, { loading, error }] = useMutation(CREATE_NOTE, {
+    refetchQueries: [{ query: GET_NOTES }],
     onCompleted: data => {
       history.push(`note/${data.createNote.id}`);
     },
