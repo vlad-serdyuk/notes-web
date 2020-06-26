8e7bddea544b1b6a7821c68ea7adc2b167963d6c
vlad.serdyuk <vladserduk@live.com>
2020-06-26 18:01:06 +0300

    Add MyNotes query

--- src/gql/query.js
+++ src/gql/query.js
@@ -25,6 +25,22 @@ export const CREATE_NOTE = gql`
   }
 `;
 
+export const GET_NOTE = gql`
+  query note($id: ID!) {
+    note(id: $id) {
+      id
+      createdAt
+      content
+      favoriteCount
+      author {
+        username
+        id
+        avatar
+      }
+    }
+  }
+`;
+
 export const GET_NOTES = gql`
   query NoteFeed($cursor: String) {
     notesFeed(cursor: $cursor) {
@@ -45,16 +61,15 @@ export const GET_NOTES = gql`
   }
 `;
 
-export const GET_NOTE = gql`
-  query note($id: ID!) {
-    note(id: $id) {
+export const GET_MY_NOTES = gql`
+  query MyNotes {
+    notes {
       id
       createdAt
       content
-      favoriteCount
       author {
-        username
         id
+        username
         avatar
       }
     }
--- src/pages/MyNotes/MyNotes.js
+++ src/pages/MyNotes/MyNotes.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import { useQuery } from '@apollo/client';
+import { GET_MY_NOTES } from '/gql/query';
+import { NoteFeed } from '/components/NoteFeed';
 
 const MyNotesPage = () => {
+  const { loading, error, data } = useQuery(GET_MY_NOTES);
+
+  if (loading) {
+    return <p>loading...</p>;
+  }
+
+  if (error) {
+    return <p>error</p>;
+  }
+
   return (
     <div>
       <h1>My notes</h1>
-      <p>My notes page</p>
+      <NoteFeed notes={data.notes} />
     </div>
   );
 };
