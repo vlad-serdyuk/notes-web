d505c698e3386625a31a1e77701144963dd55124
vlad.serdyuk <vladserduk@live.com>
2020-07-05 13:15:13 +0300

    Add skeleton to Private route

--- src/common/components/PrivateRoute.js
+++ src/common/components/PrivateRoute.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
 
+import { Skeleton } from '/components/Skeleton';
 import { useIsLoggedInQuery } from '../queries/auth';
 
 export const PrivateRoute = ({ component: Component, ...rest }) => {
     const { loading, error, data: { isLoggedIn } } = useIsLoggedInQuery();
     
-    if (loading) return <p>Loading...</p>;
+    if (loading) return <Skeleton />;
     
     if (error) return <p>Error!</p>;
     
