643316d1a3857b56abacef6e85e8828e02a1df0b
vlad.serdyuk <vladserduk@live.com>
2020-07-07 11:46:32 +0300

    Move bootstrap logic to separate service

--- src/App.js
+++ src/App.js
@@ -1,55 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Grommet } from 'grommet';
-import {
-  ApolloClient,
-  ApolloProvider,
-  InMemoryCache,
-  createHttpLink,
-} from '@apollo/client'; 
+import { ApolloProvider } from '@apollo/client'; 
 
+import GQLService from '/services/GQLService';
 import { Pages } from './pages';
-import { GET_ME, GET_APP_METADATA } from '/gql/query';
 import GlobalStyles from './styled/globalStyles';
 
-const uri = process.env.API_URI;
-const cache = new InMemoryCache();
-const httpLink = createHttpLink({ uri, credentials: 'include' });
-
-const client = new ApolloClient({
-  uri,
-  cache,
-  resolvers: {},
-  link: httpLink,
-  connectToDevTools: true,
-});
-
-const data = {
-  loading: true,
-  isLoggedIn: false,
-};
-
-client.query({ query: GET_ME }).then(({ data: userData }) => { 
-  if (userData.me) {
-    cache.writeData({ 
-      data: {
-        isLoggedIn: true,
-       },
-    });
-  }
-}).finally(() => {
-  const prevData = cache.readQuery({ query: GET_APP_METADATA });
-
-  cache.writeData({ 
-    data: {
-      ...prevData,
-      loading: false,
-     },
-  });
-});
-
-cache.writeData({ data });
-client.onResetStore(() => cache.writeData({ data }));
+const client = GQLService.getInitialClient();
+GQLService.bootstrap();
 
 const App = () => (
   <ApolloProvider client={client}>
---
+++ src/services/GQLService.js
@@ -0,0 +1,58 @@
+import {
+  ApolloClient,
+  InMemoryCache,
+  createHttpLink,
+} from '@apollo/client';
+import { GET_ME, GET_APP_METADATA } from '/gql/query';
+
+const uri = process.env.API_URI;
+const httpLink = createHttpLink({ uri, credentials: 'include' });
+
+class GQLService {
+  constructor() {
+    this.cache = new InMemoryCache();
+    this.client = new ApolloClient({
+      uri,
+      cache: this.cache,
+      resolvers: {},
+      link: httpLink,
+      connectToDevTools: true,
+    });
+  }
+
+  getInitialClient() {
+    return this.client;
+  }
+
+  bootstrap() {
+    const data = {
+      loading: true,
+      isLoggedIn: false,
+    };
+
+    this.cache.writeData({ data });
+    this.client.onResetStore(() => cache.writeData({ data }));
+
+    this.client.query({ query: GET_ME })
+      .then(({ data: userData }) => {
+        if (userData.me) {
+          this.cache.writeData({ 
+            data: {
+              isLoggedIn: true,
+            },
+          });
+        }
+      }).finally(() => {
+        const prevData = this.cache.readQuery({ query: GET_APP_METADATA });
+
+        this.cache.writeData({ 
+          data: {
+            ...prevData,
+            loading: false,
+           },
+        });
+      })
+  }
+}
+
+export default new GQLService();
