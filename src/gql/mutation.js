d69dc6759642d87d2a54f721accc8e02d97daa42
vlad.serdyuk <vladserduk@live.com>
2020-07-08 20:16:32 +0300

    Add update note mutation

--- src/gql/mutation.js
+++ src/gql/mutation.js
@@ -11,3 +11,22 @@ export const SIGNUP_USER = gql`
     signUp(email: $email, username: $username, password: $password)
   }
 `;
+
+export const UPDATE_NOTE = gql`
+  mutation updateNote($id: ID!, $content: String!) {
+    updateNote(id: $id, content: $content) {
+      id
+      content
+      createdAt
+      favoriteCount
+      favoritedBy {
+        id
+        username
+      }
+      author {
+        username
+        id
+      }
+    }
+  }
+`;
--- src/pages/EditNote/EditNote.js
+++ src/pages/EditNote/EditNote.js
@@ -2,20 +2,21 @@ import React, { useCallback } from 'react';
 import { useQuery, useMutation } from '@apollo/client';
 
 import { NoteForm } from '/components/NoteForm/NoteForm';
-import { CREATE_NOTE, GET_NOTE, GET_NOTES, GET_MY_NOTES } from '/gql/query';
+import { GET_NOTE, GET_NOTES, GET_MY_NOTES } from '/gql/query';
+import { UPDATE_NOTE } from '/gql/mutation';
 
 const EditNotePage = ({ history, match }) => {
   const { data: noteData, loading: noteLoading, error: noteError } = useQuery(GET_NOTE, { variables: { id: match.params.id } });
-  const [createNote, { loading, error }] = useMutation(CREATE_NOTE, {
+  const [updateNote, { loading, error }] = useMutation(UPDATE_NOTE, {
     refetchQueries: [{ query: GET_NOTES }, { query: GET_MY_NOTES }],
     onCompleted: data => {
-      history.push(`note/${data.createNote.id}`);
+      history.push(`note/${data.updateNote.id}`);
     },
   });
 
-  const onCreateNote = useCallback(({ note }) => {
-    createNote({ variables: { content: note } });
-  }, [createNote]);
+  const onUpdateNote = useCallback(({ note }) => {
+    updateNote({ variables: { id: noteData.note.id, content: note } });
+  }, [updateNote, noteData]);
 
   if (loading || noteLoading) {
     return <p>loading...</p>;
@@ -28,7 +29,7 @@ const EditNotePage = ({ history, match }) => {
   return (
     <NoteForm
       content={noteData.note.content}
-      submitNote={onCreateNote}
+      submitNote={onUpdateNote}
     />
   );
 };
