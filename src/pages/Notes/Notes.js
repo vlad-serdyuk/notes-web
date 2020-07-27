aca552a9b49f286a0f1102dcdb7fea98d6bc49ab
vlad.serdyuk <vladserduk@live.com>
2020-07-27 10:59:16 +0300

    Open notes by author

--- src/pages/Notes/Notes.js
+++ src/pages/Notes/Notes.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import { useQuery } from '@apollo/client';
-import { GET_ME, GET_USER_NOTES } from '/gql/query';
+import { GET_USER_NOTES } from '/gql/query';
 import { NoteFeed } from '/components/NoteFeed';
 
-const NotesPage = () => {
-  const { data: meData } = useQuery(GET_ME);
-  const { loading, error, data } = useQuery(GET_USER_NOTES, { variables: { username: meData.me.username } });
+const NotesPage = ({ match }) => {
+  const { loading, error, data } = useQuery(GET_USER_NOTES, { variables: { username: match.params.author } });
 
   if (loading) {
     return <p>loading...</p>;
