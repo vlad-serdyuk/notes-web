d6727c6904d96b59e9d5ef907ea759622024e7ce
vlad.serdyuk <vladserduk@live.com>
2020-07-28 16:42:25 +0300

    Fix styles in header avatart

--- src/components/Header/components/AvatarDropDown.js
+++ src/components/Header/components/AvatarDropDown.js
@@ -1,9 +1,17 @@
 import React, { memo } from 'react';
 import { Avatar, Box, Button, DropButton, Text } from 'grommet';
 
-const renderItems = (username, onLogOut) => (
+const renderItems = (name, username, onLogOut) => (
   <Box width="120px" background="dark-2" pad="xsmall" align="start">
-    <Text>{username}</Text>
+    <Box direction="row" gap="xsmall" border="bottom">
+      <Avatar
+        size="small"
+        background="brand"
+      >
+        {name}
+      </Avatar>
+      <Text>{username}</Text>
+    </Box>
     <Button onClick={onLogOut}>Profile</Button>
     <Button onClick={onLogOut}>Log out</Button>
   </Box>
@@ -12,7 +20,7 @@ const renderItems = (username, onLogOut) => (
 const AvatarDropButtonComponent = ({ username, name, onLogOut }) => (
   <DropButton
     alignSelf="center"
-    dropContent={renderItems(username, onLogOut)}
+    dropContent={renderItems(name, username, onLogOut)}
     dropProps={{ align: { top: "bottom" } }}
   >
     <Box height="32px" width="32px" align="center">
