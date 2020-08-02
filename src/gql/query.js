9a9a49ad96eb8fcf4716929723909018cfe621a2
vlad.serdyuk <vladserduk@live.com>
2020-08-02 20:13:48 +0300

    Add profile page

--- src/gql/query.js
+++ src/gql/query.js
@@ -19,6 +19,7 @@ export const GET_ME = gql`
       id
       email
       username
+      createdAt
     }
   }
 `
--- src/pages/Profile/Profile.js
+++ src/pages/Profile/Profile.js
@@ -1,12 +1,25 @@
 import React from 'react';
+import { format } from 'date-fns';
 import { useQuery } from '@apollo/client';
+import { Avatar, Box, Text } from 'grommet';
 
 import { GET_ME } from '/gql/query';
 
-const ProfilePage = ({ match: { params: { id } } }) => {
+const ProfilePage = () => {
   const { data: { me } = {} } = useQuery(GET_ME);
 
-  return <p>Profile</p>;
+  return (
+    <Box gap="small">
+      <Box height="40px" width="40px" align="center">
+        <Avatar background="brand">{me.username.charAt(0).toUpperCase()}</Avatar>
+      </Box>
+      <Text size="large">{me.username}</Text>
+      <Text>{me.email}</Text>
+      <Text size="small" color="grey">
+        Joined {format(me.createdAt, 'MMM do YYYY')}
+      </Text>
+    </Box>
+  );
 };
 
 export default ProfilePage;
