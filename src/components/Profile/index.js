e2866db60779c11f190507b234cf00c6529b543e
vlad.serdyuk <vladserduk@live.com>
2020-08-04 22:09:59 +0300

    Add editProfile dialog

---
+++ src/components/Profile/index.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import { format } from 'date-fns';
+import { useQuery } from '@apollo/client';
+import { Avatar, Box, Button, Text } from 'grommet';
+
+import { GET_ME } from '/gql/query';
+import { EditProfileDialog } from './EditProfileDialog';
+
+export const Profile = () => {
+  const { data: { me } = {} } = useQuery(GET_ME);
+  const [isDialogOpen, setDialogOpen] = useState(false);
+
+  const openEditProfileDialog = () => setDialogOpen(true);
+  const closeEditProfileDialog = () => setDialogOpen(false);
+
+  return (
+    <Box gap="small">
+      <Box direction="row-responsive" justify="between">
+        <Box height="40px" width="40px" align="center">
+          <Avatar background="brand">{me.username.charAt(0).toUpperCase()}</Avatar>
+        </Box>
+        <Button
+          label="Edit profile"
+          onClick={openEditProfileDialog}
+        />
+      </Box>
+      <Text size="large">{me.username}</Text>
+      <Text>{me.email}</Text>
+      <Text size="small" color="grey">
+        Joined {format(me.createdAt, 'MMM do YYYY')}
+      </Text>
+      {isDialogOpen && <EditProfileDialog onDialogClose={closeEditProfileDialog} />}
+    </Box>
+  );
+};
--- src/pages/Profile/Profile.js
+++ src/pages/Profile/Profile.js
@@ -1,25 +1,6 @@
 import React from 'react';
-import { format } from 'date-fns';
-import { useQuery } from '@apollo/client';
-import { Avatar, Box, Text } from 'grommet';
+import { Profile } from '/components/Profile';
 
-import { GET_ME } from '/gql/query';
-
-const ProfilePage = () => {
-  const { data: { me } = {} } = useQuery(GET_ME);
-
-  return (
-    <Box gap="small">
-      <Box height="40px" width="40px" align="center">
-        <Avatar background="brand">{me.username.charAt(0).toUpperCase()}</Avatar>
-      </Box>
-      <Text size="large">{me.username}</Text>
-      <Text>{me.email}</Text>
-      <Text size="small" color="grey">
-        Joined {format(me.createdAt, 'MMM do YYYY')}
-      </Text>
-    </Box>
-  );
-};
+const ProfilePage = () => <Profile />;
 
 export default ProfilePage;
