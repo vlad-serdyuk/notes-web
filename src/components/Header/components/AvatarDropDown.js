6ca03ec3d94d10f26c9ce04e674f7c76e3af0331
vlad.serdyuk <vladserduk@live.com>
2020-08-10 13:20:23 +0300

    Fix closing dialog by clicking item

--- src/components/Header/components/AvatarDropDown.js
+++ src/components/Header/components/AvatarDropDown.js
@@ -1,9 +1,9 @@
-import React, { memo } from 'react';
+import React, { memo, useState, useCallback } from 'react';
 import { Avatar, Box, DropButton, Text } from 'grommet';
 
 import { StyledButton } from './AvatarDropDown.styled';
 
-const renderItems = (email, initials, username, openProfilePage, onLogOut) => (
+const renderItems = (email, initials, username, onProfileClick, onLogOut) => (
   <Box width="160px" background="dark-2" pad="xsmall" align="start">
     <Box direction="row" align="center" gap="xsmall" border="bottom">
       <Avatar
@@ -17,21 +17,33 @@ const renderItems = (email, initials, username, openProfilePage, onLogOut) => (
         <Text size="small">{email}</Text>
       </Box>
     </Box>
-    <StyledButton onClick={openProfilePage}>Profile</StyledButton>
+    <StyledButton onClick={onProfileClick}>Profile</StyledButton>
     <StyledButton onClick={onLogOut}>Log out</StyledButton>
   </Box>
 );
 
-const AvatarDropButtonComponent = ({ email, username, initials, openProfilePage, onLogOut }) => (
-  <DropButton
-    alignSelf="center"
-    dropContent={renderItems(email, initials, username, openProfilePage, onLogOut)}
-    dropProps={{ align: { top: "bottom" } }}
-  >
-    <Box height="32px" width="32px" align="center">
-      <Avatar background="brand">{initials}</Avatar>
-    </Box>
-  </DropButton>
-);
+const AvatarDropButtonComponent = ({ email, username, initials, openProfilePage, onLogOut }) => {
+  const [open, setOpen] = useState(false);
+
+  const onProfileClick = useCallback(() => {
+    openProfilePage();
+    setOpen(false);
+  }, [open, setOpen, openProfilePage]);
+
+  return (
+    <DropButton
+      open={open}
+      onClose={() => setOpen(false)}
+      onOpen={() => setOpen(true)}
+      alignSelf="center"
+      dropContent={renderItems(email, initials, username, onProfileClick, onLogOut)}
+      dropProps={{ align: { top: "bottom" } }}
+    >
+      <Box height="32px" width="32px" align="center">
+        <Avatar background="brand">{initials}</Avatar>
+      </Box>
+    </DropButton>
+  );
+}
 
 export const AvatarDropButton = memo(AvatarDropButtonComponent);
