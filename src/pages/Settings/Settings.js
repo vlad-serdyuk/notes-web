b000d57130d6fa40f5546fc5c952578664511ee0
vlad.serdyuk <vladserduk@live.com>
2020-09-27 21:38:24 +0300

    Add reset password logic

--- src/pages/Settings/Settings.js
+++ src/pages/Settings/Settings.js
@@ -1,4 +1,4 @@
-import React, { useState, useCallback } from 'react';
+import React, { useState, useCallback, useEffect } from 'react';
 import { useMutation } from '@apollo/client';
 import { Box, Button, Heading, Text, TextInput } from 'grommet';
 
@@ -8,6 +8,7 @@ export const SettingsPage = () => {
   const [oldPassword, setOldPassword] = useState('');
   const [newPassword, setNewPassword] = useState('');
   const [confirmedNewPassword, setConfirmedNewPassword] = useState('');
+  const [disabled, setDisabled] = useState(true);
 
   const [resetPasswordMutation, { loading, error }] = useMutation(RESET_PASSWORD, {
     onCompleted: data => {
@@ -19,31 +20,42 @@ export const SettingsPage = () => {
     resetPasswordMutation({ variables: { oldPassword, newPassword } });
   }, [oldPassword, newPassword, confirmedNewPassword]);
 
+  const onChangeOldPassword = (event) => setOldPassword(event.target.value);
+  const onChangeNewPassword = (event) => setNewPassword(event.target.value);
+  const onChangeConfirmPassword = (event) => setConfirmedNewPassword(event.target.value);
+
+  useEffect(() => {
+    setDisabled(!newPassword || newPassword !== confirmedNewPassword);
+  }, [setDisabled, newPassword, confirmedNewPassword]);
+
   return (
-    <Box>
-      <Heading>Reset password</Heading>
+    <Box gap="small">
+      <Text size="large">Reset password</Text>
       <Text>Old password</Text>
       <TextInput
         type="password"
         placeholder="Old password"
         value={oldPassword}
+        onChange={onChangeOldPassword}
       />
       <Text>New password</Text>
       <TextInput
         type="password"
         placeholder="New password"
         value={newPassword}
+        onChange={onChangeNewPassword}
       />
       <Text>Confirm password</Text>
       <TextInput
         type="password"
         placeholder="Confirm password"
         value={confirmedNewPassword}
+        onChange={onChangeConfirmPassword}
       />
       <Button
         primary
         label="Save"
-        disabled={true}
+        disabled={disabled}
         onClick={resetPassword}
       />
     </Box>
