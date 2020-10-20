e790512ca4d7ca528cb49e414cab340b0a2df56a
vlad.serdyuk <vladserduk@live.com>
2020-10-20 21:24:10 +0300

    Add search bar logic

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState, useCallback, useRef } from 'react';
 import { withRouter } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import { Search } from 'grommet-icons';
 import { Box, Image, Text, TextInput } from 'grommet';
 
 const renderSuggestions = () => {
@@ -32,21 +33,58 @@ const renderSuggestions = () => {
 };
 
 const SearchBarComponent = ({ history }) => {
+  const [value, setValue] = useState('');
+  const [isSuggestionOpen, setSuggestionOpen] = useState(false);
+  const [suggestedFolks, setSuggestedFolks] = useState([]);
+
+  const boxRef = useRef();
+
+  const setOpen = useCallback(() => {
+    setSuggestionOpen(true);
+  }, [setSuggestionOpen]);
+
+  const setClose = useCallback(() => {
+    setSuggestionOpen(false);
+  }, [setSuggestionOpen]);
+
   return (
-    <Box>
-      <TextInput
-        type="search"
-        dropTarget={boxRef.current}
-        plain
-        value={value}
-        suggestions={renderSuggestions()}
-        placeholder="Search Notedly"
-        onChange={onChange}
-        onSelect={onSelect}
-        onSuggestionsOpen={() => setSuggestionOpen(true)}
-        onSuggestionsClose={() => setSuggestionOpen(false)}
-      />
-    </Box>
+    <Box background="dark-1" fill align="center" pad={{ top: 'large' }}>
+        <Box
+          ref={boxRef}
+          width="large"
+          direction="row"
+          align="center"
+          pad={{ horizontal: 'small', vertical: 'xsmall' }}
+          round="small"
+          elevation={suggestionOpen ? 'medium' : undefined}
+          border={{
+            side: 'all',
+            color: suggestionOpen ? 'transparent' : 'border',
+          }}
+          style={
+            suggestionOpen
+              ? {
+                  borderBottomLeftRadius: '0px',
+                  borderBottomRightRadius: '0px',
+                }
+              : undefined
+          }
+        >
+          <Search color="brand" />
+          <TextInput
+            type="search"
+            dropTarget={boxRef.current}
+            plain
+            value={value}
+            onChange={onChange}
+            onSelect={onSelect}
+            suggestions={renderSuggestions()}
+            placeholder="Search notedly"
+            onSuggestionsOpen={setOpen}
+            onSuggestionsClose={setClose}
+          />
+        </Box>
+      </Box>
   );
 };
 
