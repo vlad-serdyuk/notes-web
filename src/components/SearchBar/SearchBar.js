c667f1f82121770c7acca84b56ec0995fb70371d
vlad.serdyuk <vladserduk@live.com>
2020-10-21 22:54:50 +0300

    Add searchbar logic

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -4,38 +4,10 @@ import PropTypes from 'prop-types';
 import { Search } from 'grommet-icons';
 import { Box, Image, Text, TextInput } from 'grommet';
 
-const renderSuggestions = () => {
-  return []
-    .filter(
-      ({ name }) => name.toLowerCase().indexOf(value.toLowerCase()) >= 0,
-    )
-    .map(({ name, imageUrl }, index, list) => ({
-      label: (
-        <Box
-          direction="row"
-          align="center"
-          gap="small"
-          border={index < list.length - 1 ? 'bottom' : undefined}
-          pad="small"
-        >
-          <Image
-            width="48px"
-            src={imageUrl}
-            style={{ borderRadius: '100%' }}
-          />
-          <Text>
-            <strong>{name}</strong>
-          </Text>
-        </Box>
-      ),
-      value: name,
-    }));
-};
-
 const SearchBarComponent = ({ history }) => {
   const [value, setValue] = useState('');
   const [isSuggestionOpen, setSuggestionOpen] = useState(false);
-  const [suggestedFolks, setSuggestedFolks] = useState([]);
+  const [suggestedResults, setSuggestedResults] = useState([]);
 
   const boxRef = useRef();
 
@@ -44,14 +16,42 @@ const SearchBarComponent = ({ history }) => {
     setValue(newValue);
 
     if (!newValue.trim()) {
-      setSuggestedFolks([]);
+      setSuggestedResults([]);
     } else {
       // simulate an async call to the backend
-      setTimeout(() => setSuggestedFolks(folks), 300);
+      setTimeout(() => setSuggestedResults(folks), 300);
     }
   };
 
   const onSelect = event => setValue(event.suggestion.value);
+
+  const renderSuggestions = () => {
+    return suggestedResults
+      .filter(
+        ({ name }) => name.toLowerCase().indexOf(value.toLowerCase()) >= 0,
+      )
+      .map(({ name, imageUrl }, index, list) => ({
+        label: (
+          <Box
+            direction="row"
+            align="center"
+            gap="small"
+            border={index < list.length - 1 ? 'bottom' : undefined}
+            pad="small"
+          >
+            <Image
+              width="48px"
+              src={imageUrl}
+              style={{ borderRadius: '100%' }}
+            />
+            <Text>
+              <strong>{name}</strong>
+            </Text>
+          </Box>
+        ),
+        value: name,
+      }));
+  };
 
   const setOpen = useCallback(() => {
     setSuggestionOpen(true);
@@ -70,18 +70,18 @@ const SearchBarComponent = ({ history }) => {
           align="center"
           pad={{ horizontal: 'small', vertical: 'xsmall' }}
           round="small"
-          elevation={suggestionOpen ? 'medium' : undefined}
+          elevation={isSuggestionOpen ? 'medium' : null}
           border={{
             side: 'all',
-            color: suggestionOpen ? 'transparent' : 'border',
+            color: isSuggestionOpen ? 'transparent' : 'border',
           }}
           style={
-            suggestionOpen
+            isSuggestionOpen
               ? {
                   borderBottomLeftRadius: '0px',
                   borderBottomRightRadius: '0px',
                 }
-              : undefined
+              : null
           }
         >
           <Search color="brand" />
