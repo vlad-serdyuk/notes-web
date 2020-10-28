2c2cdad98d9b0c6d3f808fa98b98b89ebad4b92f
vlad.serdyuk <vladserduk@live.com>
2020-10-28 10:37:16 +0300

    Add some styles to trends widget component

--- src/components/TrendsWidget/TrendsWidget.styled.js
+++ src/components/TrendsWidget/TrendsWidget.styled.js
@@ -3,7 +3,7 @@ import { Box, Heading } from 'grommet';
 
 export const TrendsContainer = styled(Box).attrs({
   round: 'medium',
-  gap: 'medium',
+  gap: 'xsmall',
   border: { side: 'between' },
   pad: '16px 0'
 })``;
@@ -16,8 +16,10 @@ export const TrendsHeader = styled(Heading).attrs({
 `;
 
 export const TrendBlock = styled(Box).attrs({
-  pad: '0 8px',
+  pad: '2px 8px',
 })`
+  margin: -2px 0;
+  
   &:hover {
     cursor: pointer;
     background-color: rgba(120, 120, 120, 0.05);
