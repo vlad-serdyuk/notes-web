2b847591e66fceb164ea7c56a6fcfbeb33105e91
vlad.serdyuk <vladserduk@live.com>
2020-11-04 11:52:28 +0300

    Add webpck config

--- internals/webpack.config.js
+++ internals/webpack.config.js
@@ -1,11 +1,30 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: path.resolve(__dirname, './src/index.js'),
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.ts?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.js' ],
+  },
   output: {
     path: path.resolve(__dirname, './dist'),
     filename: 'bundle.js'
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+        template: path.join(__dirname, 'src', 'index.html')
+    })
+  ],
   devServer: {
     contentBase: path.resolve(__dirname, './dist'),
   }
