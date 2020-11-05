5d9ab2f30bf6ae72386de79a1f14a98131c62f2a
vlad.serdyuk <vladserduk@live.com>
2020-11-05 22:12:07 +0300

    Add babel-loader

--- internals/webpack.config.js
+++ internals/webpack.config.js
@@ -2,15 +2,20 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: path.resolve(__dirname, './src/index.js'),
+  entry: path.resolve(__dirname, '../src/App.js'),
   devtool: 'inline-source-map',
   module: {
     rules: [
       {
+        test: /\.js?$/,
+        use: 'babel-loader',
+        exclude: /node_modules/,
+      },
+      /* {
         test: /\.ts?$/,
         use: 'ts-loader',
         exclude: /node_modules/,
-      },
+      }, */
     ],
   },
   resolve: {
