12feb7d2e3df359c0228e4c3675601850c26d780
vlad-serdyuk <vladserduk@live.com>
2020-12-15 23:03:48 +0100

    add display settings to settings page

--- src/pages/Settings/Settings.tsx
+++ src/pages/Settings/Settings.tsx
@@ -1,12 +1,18 @@
 import React, { FC } from 'react';
 import { Accordion, AccordionPanel, Box } from 'grommet';
-import { ResetPasswordForm } from '../../components/ResetPasswordForm/ResetPasswordForm';
+import { DisplaySettings } from '../../components/DisplaySettings';
+import { ResetPasswordForm } from '../../components/ResetPasswordForm';
 
 export const SettingsPage: FC = () => (
   <Accordion animate={false}>
     <AccordionPanel label="Reset password">
       <Box pad="small" height="large" flex={false}>
         <ResetPasswordForm />
+      </Box>
+    </AccordionPanel>
+    <AccordionPanel label="Display">
+      <Box pad="small" height="large" flex={false}>
+        <DisplaySettings />
       </Box>
     </AccordionPanel>
   </Accordion>
