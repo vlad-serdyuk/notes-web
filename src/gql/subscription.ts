71f44cf616b05e1baa9fa3c8a0a5263d58e59013
vlad-serdyuk <vladserduk@live.com>
2021-03-12 21:43:34 +0100

    Add subscription gql

---
+++ src/gql/subscription.ts
@@ -0,0 +1,11 @@
+import { gql } from '@apollo/client';
+import { NOTE_FRAGMENT } from './fragments';
+
+const COMMENTS_SUBSCRIPTION = gql`
+  subscription notesFeedUpdated() {
+    notesFeedUpdated() {
+        ...NoteEntity
+    }
+  }
+  ${NOTE_FRAGMENT}
+`;
